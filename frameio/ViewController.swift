75066954c5d3aef8ca9eb8931af4bf42a2218689
Vlad Z <vladislavz1991@gmail.com>
2020-01-14 15:19:17 -0500

    init

---
+++ frameio/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  frameio
+//
+//  Created by Vlad Z. on 1/14/20.
+//  Copyright © 2020 Vlad Z. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
